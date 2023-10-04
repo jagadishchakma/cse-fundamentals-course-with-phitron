2dee542320ad95504391c2f2e45890d281cdbcc0
jagadishchakma <jagadishchakma401@gmail.com>
2023-10-04 06:40:21 +0600

    c operator,conditions

--- module_02_operator_conditions/condition1.c
+++ module_02_operator_conditions/condition1.c
@@ -5,7 +5,7 @@ int main(){
     scanf("%d", &tk);
 
     if(tk >= 100){
-        printf("I will be eat Burger!\n");
+        printf("I will be eat Burger more!\n");
     }else{
         printf("Not eat Burger!\n");
     }
