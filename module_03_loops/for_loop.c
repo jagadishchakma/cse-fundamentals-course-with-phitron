05010421bb09665cbbb3788fae5dc702b7c5e2e0
jagadishchakma <jagadishchakma401@gmail.com>
2023-10-04 06:41:39 +0600

    c loops

--- module_03_loops/for_loop.c
+++ module_03_loops/for_loop.c
@@ -2,9 +2,9 @@
 int main(){
     for(int i = 1; i <=3; i=i+1){
         if(i%2 == 0){
-            printf("Even: %d\n", i);
+            printf("even: %d\n", i);
         }else{
-           printf("Odd: %d\n", i);
+           printf("odd: %d\n", i);
         }
     };
     return 0;
